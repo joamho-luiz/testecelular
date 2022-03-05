c1e5fff3723072bd5c7dbfb6fd9a15b37daaa6a8
João Luiz de Lara <72744520+joamho-luiz@users.noreply.github.com>
2022-03-05 09:26:05 -0300

    teste
    alteramos nomes de arquivos

---
+++ canais-gerador.js
@@ -0,0 +1,32 @@
+// Variáveis vindas de dados
+import {canais} from './dados.js'
+
+for (let posiccamho in canais) {
+    // Criação dos elementos    
+    var article  = document.createElement('article')
+    var foto = document.createElement('div')
+    var img = document.createElement('img')
+    var texto = document.createElement('div')
+    var a = document.createElement('a')
+    var p = document.createElement('p')
+
+    // Classe para alguns elementos
+    foto.className = 'foto'
+    texto.className = 'texto'
+
+    // Outros atributos
+    a.innerHTML = canais[posiccamho][0]
+    p.innerHTML = canais[posiccamho][1]
+    foto.style.backgroundImage = "url('" + canais[posiccamho][2] + "')"
+    a.href = canais[posiccamho][3]
+
+    // Hierarquização dos elementos
+    article.appendChild(foto)
+    article.appendChild(texto)
+    foto.appendChild(img)
+    texto.appendChild(a)
+    texto.appendChild(p)
+
+    // Adição da seção
+    document.querySelector('#seccamho').appendChild(article)
+}
